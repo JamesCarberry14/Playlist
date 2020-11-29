03928e18340fc4ca934844d28c1241c5593f508b
James Carberry <S00197582@mail.itsligo.ie>
2020-11-29 19:14:39 +0000

    Added 5 song objects and a playlist

--- Playlist/Program.cs
+++ Playlist/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using static System.Console;
 
 namespace Playlist
 {
@@ -6,7 +9,26 @@ namespace Playlist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Song s1 = new Song("Beautiful People", "ED SHEERAN",3.15,Genre.Pop);
+            Song s2 = new Song("Ride It", "REGARD", 3.37, Genre.Dance);
+            Song s3 = new Song("Dance Monkey", "TONES & I", 4.20, Genre.Dance);
+            Song s4 = new Song("Circles", "POST MALONE", 3.25, Genre.Pop);
+            Song s5 = new Song("South Of The Border", "ED SHEERAN", 4.26, Genre.Other);
+
+            List<Song> playlist = new List<Song>();
+
+            playlist.Add(s1);
+            playlist.Add(s2);
+            playlist.Add(s3);
+            playlist.Add(s4);
+            playlist.Add(s5);
+
+            foreach (Song song in playlist)
+            {
+                WriteLine(song);
+            }
+
+
         }
     }
 }
--- Playlist/Song.cs
+++ Playlist/Song.cs
@@ -6,8 +6,36 @@ namespace Playlist
 {
     class Song
     {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
 
+
+        public Genre MusicGenre { get; set; }
+
+
+        public Song(string title, string artist, double duration, Genre musicGenre )
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            MusicGenre = musicGenre;
+        }
+
+        public Song(string title, string artist) : this(title, artist, 0, Genre.Other) { }
+
+
+        public Song() : this("Unknown", "Unknown") { }
+
+
+
+        public override string ToString()
+        {
+            return string.Format($"{Title} {Artist} {Duration} {MusicGenre}");
+        }
 
 
     }
+
+    public enum Genre {Rock, Pop, Dance, Other }
 }
